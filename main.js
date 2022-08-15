4701441417cb09d5b34ed542ff4d8c1d96a201b2
Moradabad <75120768+masterbreaker@users.noreply.github.com>
2022-08-15 15:48:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,3 +17,43 @@ function take_snapshot(){
 
     }); 
 }
+
+console.log("ml5 version: ",ml5.version);
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Mzd38Cm-a/model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("modelloaded");
+
+}
+
+function check(){
+    img=document.getElementById("capture_image");
+    classifier.classify(img,gotResult);
+
+    
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+
+    }
+
+    else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+
+        prediction_1=results[0].label;
+        speak();
+    }
+}
+
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data_1="the first prediction is "+prediction_1;
+
+    var utterThis=new SpeechSynthesisUtterance(speak_data_1);
+    synth.speak(utterThis);
+}
+
